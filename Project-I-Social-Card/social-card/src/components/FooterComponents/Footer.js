1024180b01d4ed428c95e81f9d9a053e7b1fcb00
abidex4yemi <abidex4yemi@gmail.com>
2019-05-27 22:12:35 +0100

    Add card footer structure

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,18 @@ export const Footer = () => {
 	return (
 		<footer className="card-footer">
 			<div className="footer-content">
-				<a href="#!">footerText</a>
+				<a href="#!" className="icon-link">
+					<i class="far fa-comment" />
+				</a>
+				<a href="#!" className="icon-link">
+					<i class="fas fa-retweet" />
+				</a>
+				<a href="#!" className="icon-link">
+					<i class="far fa-heart" />
+				</a>
+				<a href="#!" className="icon-link">
+					<i class="far fa-envelope" />
+				</a>
 			</div>
 		</footer>
 	);
