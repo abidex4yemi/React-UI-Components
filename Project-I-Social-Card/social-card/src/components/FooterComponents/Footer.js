a313720e55f4322e776d1624a0c96ba121c03ac2
abidex4yemi <abidex4yemi@gmail.com>
2019-05-27 22:59:02 +0100

    Add click event handler to card icon

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Footer.css';
 
-export const Footer = () => {
-	return (
-		<footer className="card-footer">
-			<div className="footer-content">
-				<a href="#!" className="icon-link">
-					<i class="far fa-comment" />
-				</a>
-				<a href="#!" className="icon-link">
-					<i class="fas fa-retweet" />
-				</a>
-				<a href="#!" className="icon-link">
-					<i class="far fa-heart" />
-				</a>
-				<a href="#!" className="icon-link">
-					<i class="far fa-envelope" />
-				</a>
-			</div>
-		</footer>
-	);
-};
+export class Footer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			comment: 0,
+			retweet: 0,
+			like: 0,
+			message: 0
+		};
+	}
+
+	handleClick(evt) {
+		this.setState(prevState => ({
+			[evt]: prevState[evt] + 1
+		}));
+	}
+
+	render() {
+		const { comment, retweet, like, message } = this.state;
+
+		return (
+			<footer className="card-footer">
+				<div className="footer-content">
+					<i className="far fa-comment icon" onClick={() => this.handleClick('comment')}>
+						{' '}
+						{comment}
+					</i>
+					<i className="fas fa-retweet icon" onClick={() => this.handleClick('retweet')}>
+						{' '}
+						{retweet}
+					</i>
+					<i className="far fa-heart icon" onClick={() => this.handleClick('like')}>
+						{' '}
+						{like}
+					</i>
+					<i className="far fa-envelope icon" onClick={() => this.handleClick('message')}>
+						{' '}
+						{message}
+					</i>
+				</div>
+			</footer>
+		);
+	}
+}
