e8630c89050bcd665a335f1ef75f7aeaca9d12ff
abidex4yemi <abidex4yemi@gmail.com>
2019-05-28 12:45:14 +0100

    Create NumberButton component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+export const NumberButton = props => {
+	const { buttonStyle, buttonText, buttonContainerStyle } = props;
+	return (
+		<td className={buttonContainerStyle}>
+			<button className={buttonStyle}>{buttonText}</button>
+		</td>
+	);
+};
