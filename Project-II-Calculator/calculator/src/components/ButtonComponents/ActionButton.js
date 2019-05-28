bb576e121295fba36cab424fe80de5d24e7b7bf4
abidex4yemi <abidex4yemi@gmail.com>
2019-05-28 12:49:33 +0100

    create ActionButton component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+export const ActionButton = props => {
+	const { buttonStyle, buttonText, buttonContainerStyle } = props;
+	return (
+		<td className={buttonContainerStyle}>
+			<button className={buttonStyle}>{buttonText}</button>
+		</td>
+	);
+};
